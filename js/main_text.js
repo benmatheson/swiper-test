6da2a309cd04fcc9b951a4b7fb968d069ea4f273
benmatheson <mathesonben@gmail.com>
2022-04-07 17:55:29 -0800

    initial

---
+++ js/main_text.js
@@ -0,0 +1,52 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  effect: "cards",
+
+  // cardsEffect: {
+  // slideShadows: false
+  // },
+  // effect: "fade",
+  // effect: "flip",
+  slideToClickedSlide: true,
+  // speed: 10,
+  // zoom: true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+
+  // Navigation arrows
+  // navigation: false,
+  navigation: 
+  
+  { 
+    // nextEl: '.swiper-button-next',
+    nextEl: '.swiper-wrapper',
+    // prevEl: '.swiper-button-prev'
+  }
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   // el: '.swiper-scrollbar',
+  // },
+});
+
+
+// function full () {
+
+// var elem = document.getElementById("swiper");
+
+// elem.webkitRequestFullscreen();
+
+// }
+
+
+// fullElement = document.getElementById("full")
+
+// fullElement.addEventListener("click", full)
